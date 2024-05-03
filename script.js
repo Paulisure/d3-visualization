2c0283416400ff480396ff843709fdeade65b6b4
Paul Loupe <145220010+Paulisure@users.noreply.github.com>
2024-05-03 09:37:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,83 +15,50 @@ document.addEventListener('DOMContentLoaded', async function() {
   variables.forEach(variable => {
     const value = d => +d[variable];
     const domain = d3.extent(data, value);
-    xScale[variable] = d3.scaleLinear(domain, [padding / 2, size - padding / 2]);
-    yScale[variable] = d3.scaleLinear(domain, [size - padding / 2, padding / 2]);
+    xScale[variable] = d3.scaleLinear().domain(domain).range([padding / 2, size - padding / 2]);
+    yScale[variable] = d3.scaleLinear().domain(domain).range([size - padding / 2, padding / 2]);
   });
 
-  const xAxis = d3.axisBottom()
-    .ticks(6)
-    .tickSize(size * variables.length);
+  // Creating axes
+  const xAxis = d3.axisBottom().ticks(6).tickSize(size * variables.length);
+  const yAxis = d3.axisLeft().ticks(6).tickSize(-size * variables.length);
 
-  const yAxis = d3.axisLeft()
-    .ticks(6)
-    .tickSize(-size * variables.length);
-
-  svg.selectAll(".x.axis")
-    .data(variables)
-    .join("g")
-    .attr("class", "x axis")
-    .attr("transform", (d, i) => `translate(${i * size},${size * variables.length})`)
-    .each(function(d) { d3.select(this).call(xAxis.scale(xScale[d])); });
-
-  svg.selectAll(".y.axis")
-    .data(variables)
-    .join("g")
-    .attr("class", "y axis")
-    .attr("transform", (d, i) => `translate(0,${i * size})`)
-    .each(function(d) { d3.select(this).call(yAxis.scale(yScale[d])); });
-
-  // Create scatter plots
-  const cell = svg.selectAll("g.cell")
+  // Creating cells for each pair of variables
+  const cell = svg.selectAll(".cell")
     .data(d3.cross(variables, variables))
     .join("g")
     .attr("class", "cell")
-    .attr("transform", ([i, j]) => `translate(${i * size},${j * size})`);
+    .attr("transform", ([x, y], i) => `translate(${variables.indexOf(x) * size},${variables.indexOf(y) * size})`);
 
-  cell.each(function([i, j]) {
-    const x = xScale[i];
-    const y = yScale[j];
-
+  // Filling cells with circles
+  cell.each(function([x, y]) {
     d3.select(this).selectAll("circle")
       .data(data)
       .join("circle")
-      .attr("cx", d => x(d[i]))
-      .attr("cy", d => y(d[j]))
+      .attr("cx", d => xScale[x](d[x]))
+      .attr("cy", d => yScale[y](d[y]))
       .attr("r", 3)
-      .attr("fill", d => d.DEATH_EVENT ? "red" : "green");
+      .attr("fill", d => d.DEATH_EVENT === "1" ? "red" : "green");
   });
 
-  // Add brushing functionality
-  const brush = d3.brush()
+  // Implement brushing
+  cell.call(d3.brush()
     .extent([[padding / 2, padding / 2], [size - padding / 2, size - padding / 2]])
-    .on("start", brushstarted)
-    .on("brush", brushed);
-
-  cell.call(brush);
-
-  let brushCell;
-
-  // Clear the previously-active brush, if any.
-  function brushstarted() {
-    if (brushCell !== this) {
-      d3.select(brushCell).call(brush.move, null);
-      brushCell = this;
-    }
-  }
-
-  // Highlight the selected circles.
-  function brushed({selection}, [i, j]) {
-    let selected = [];
-    if (selection) {
-      const [[x0, y0], [x1, y1]] = selection;
-      const x = xScale[i];
-      const y = yScale[j];
-      selected = data.filter(d =>
-        x0 <= x(d[i]) && x(d[i]) <= x1 &&
-        y0 <= y(d[j]) && y(d[j]) <= y1
-      );
-    }
-    cell.selectAll("circle")
-      .classed("hidden", d => !selected.includes(d));
-  }
-});
+    .on("start brush", function(event) {
+    .on("start brush", function(event, [x, y]) {
+      const selection = event.selection;
+      if (selection) {
+        const [[x0, y0], [x1, y1]] = selection;
+        svg.selectAll("circle").classed("hidden", d => {
+          return x0 > xScale[x](d[x]) || x1 < xScale[x](d[x]) ||
+                 y0 > yScale[y](d[y]) || y1 < yScale[y](d[y]);
+        });
+      } else {
+        svg.selectAll("circle").classed("hidden", false);
+      }
+    })
+    .on("end", function(event, [x, y]) {
+      if (!event.selection) {
+        svg.selectAll("circle").classed("hidden", false);
+      }
+    });
